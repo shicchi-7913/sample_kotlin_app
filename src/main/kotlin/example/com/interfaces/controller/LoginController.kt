5a733a527d4e87edd1a01072784e8be4070bd3ef
Shichida.Masatsugu <shicchi.7913@icloud.com>
2024-09-01 01:58:54 +0900

    add password authentication

--- src/main/kotlin/example/com/interfaces/controller/LoginController.kt
+++ src/main/kotlin/example/com/interfaces/controller/LoginController.kt
@@ -1,16 +1,42 @@
 package example.com.interfaces.controller
 
+import at.favre.lib.crypto.bcrypt.BCrypt
 import example.com.interfaces.form.LoginRequest
+import example.com.model.Users
+import example.com.model.Users.email
+import example.com.model.Users.password
 import example.com.plugins.UserSession
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.ApplicationCall
+import io.ktor.http.*
+import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.sessions.*
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
 
 class LoginController {
     suspend fun post(loginRequest: LoginRequest, call: ApplicationCall) {
-        if (loginRequest.email == "hoge@example.com" && loginRequest.password == "foobar") {
-            call.sessions.set(UserSession(id = loginRequest.email))
+        val user = transaction {
+            Users
+                .selectAll()
+                .where { Users.email.eq(loginRequest.email!!) }
+                .firstOrNull()
+                ?.let { row ->
+                    mapOf(
+                        "email" to row[Users.email],
+                        "password" to row[Users.password]
+                    )
+                }
+        }
+
+        if (user == null) {
+            call.respond(HttpStatusCode.NotFound)
+            return
+        }
+
+        val result: BCrypt.Result = BCrypt.verifyer().verify(loginRequest.password?.toCharArray(), user["password"])
+
+        if (result.verified) {
+            call.sessions.set(UserSession(id = user["email"]!!))
             call.respond(HttpStatusCode.OK)
         } else {
             call.respond(HttpStatusCode.Unauthorized, "email or password is invalid")
